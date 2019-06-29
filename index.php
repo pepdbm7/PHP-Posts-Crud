8a323a09eb9d4c0e97776c8a544192e0a25be401
pepdbm7 <pepdbm7@gmail.com>
2019-06-29 20:20:35 +0200

    created form

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Crud</title>
+</head>
+<body>
+
+<div class="mx-auto" style="width: 300px;">
+    <form action="" method="POST">
+        <div class="form-group">
+        <label for="Title">Title</label>
+        <input type="text" id="Title" class="form-control" placeholder=" Enter a title">
+        </div>
+        <div class="form-group">
+        <label for="content">Content</label>
+        <input type="text" id="content" class="form-control" rows="3" placeholder="Write the content...">
+        </div>
+        <div class="form-group">
+        <button type="submit" class="btn btn-primary">Create</button>
+        </div>
+    </form>
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
